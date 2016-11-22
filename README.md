f433ad630510b770d25d47a1336b9b37319c08de
Lior Kooznits <Liork@888holdings.corp>
2016-11-22 21:10:36 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+How to start
+
+In order to start the seed use:
+
+git clone --depth 1 https://github.com/Liork1/MVC-CORE-Seed
---
+++ src/TodoApi/Controllers/LocaleController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using TodoApi.Models;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+
+namespace TodoApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class LocaleController : Controller
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            //string st = System.IO.File.ReadAllText("Data/locale.json");
+
+            JObject o = getJson();
+                //JObject.Parse(st);
+            return new ObjectResult(o);
+        }
+
+        [HttpGet("{lang}")]
+        public IActionResult GetByLang(string lang)
+        {
+            JObject obj = getJson();
+
+            foreach (var x in obj)
+            {
+                string name = x.Key;
+                if (name.ToLowerInvariant() == lang.ToLowerInvariant())
+                {
+                    return new ObjectResult(x.Value);
+                }
+            }
+
+            return new ObjectResult(String.Empty);
+        }
+
+        private JObject getJson()
+        {
+            string st = System.IO.File.ReadAllText("Data/locale.json");
+            return JObject.Parse(st);
+        }
+    }
+
+}
